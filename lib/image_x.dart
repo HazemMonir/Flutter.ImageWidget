9823bc9a5059dc433d77c5d4cd37243006d6fbd0
HazemMonir <hazem.monir@gmail.com>
2023-01-22 16:28:17 +0200

    initial release

--- lib/image_x.dart
+++ lib/image_x.dart
@@ -4,15 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:universal_io/io.dart';
 
-/// check if string is url
-bool isUrl(String url) => Uri.parse(url).host.isNotEmpty;
-
-/// check if path starts with assets/
-bool isAsset(String path) => path.startsWith('assets/');
-
-/// check if path contains .svg
-bool isSVG(String path) => path.contains('.svg');
-
 /// Lazy image widget for any path [Asset, Path, Url] for all image types
 class ImageX extends StatelessWidget {
   /// required path to image
@@ -39,41 +30,51 @@ class ImageX extends StatelessWidget {
     this.svgColor,
   });
 
+  /// check if string is url
+  bool _isUrl(String url) => Uri.parse(url).host.isNotEmpty;
+
+  /// check if path starts with assets/
+  bool _isAsset(String path) => path.startsWith('assets/');
+
+  /// check if path contains .svg
+  bool _isSVG(String path) => path.contains('.svg');
+
+  Widget _errorWidgetBuilder(_, __, ___) =>
+      loadingErrorWidget ?? const Icon(Icons.close_rounded, size: 50);
+
+  Widget loadingWidgetBuilder(_, __, ___) => loadWidget!;
+
+  _networkImage() => _isSVG(path)
+      ? SvgPicture.network(path, color: svgColor)
+      : Image.network(
+          path,
+          fit: fit,
+          errorBuilder: _errorWidgetBuilder,
+          loadingBuilder: loadingWidgetBuilder,
+        );
+
+  _fileImage() => _isSVG(path)
+      ? SvgPicture.file(File(path), color: svgColor)
+      : Image.file(
+          File(path),
+          fit: fit,
+          errorBuilder: _errorWidgetBuilder,
+        );
+
+  _assetImage() => _isSVG(path)
+      ? SvgPicture.asset(path, color: svgColor)
+      : Image.asset(
+          path,
+          fit: fit,
+          errorBuilder: _errorWidgetBuilder,
+        );
+
   @override
   Widget build(BuildContext context) {
-    Widget errorWidgetBuilder(_, __, ___) =>
-        loadingErrorWidget ?? const Icon(Icons.close_rounded, size: 50);
-
-    Widget loadingWidgetBuilder(_, __, ___) => loadWidget!;
-
-    networkImage() => isSVG(path)
-        ? SvgPicture.network(path, color: svgColor)
-        : Image.network(
-            path,
-            fit: fit,
-            errorBuilder: errorWidgetBuilder,
-            loadingBuilder: loadingWidgetBuilder,
-          );
-
-    fileImage() => isSVG(path)
-        ? SvgPicture.file(File(path), color: svgColor)
-        : Image.file(
-            File(path),
-            fit: fit,
-            errorBuilder: errorWidgetBuilder,
-          );
-    assetImage() => isSVG(path)
-        ? SvgPicture.asset(path, color: svgColor)
-        : Image.asset(
-            path,
-            fit: fit,
-            errorBuilder: errorWidgetBuilder,
-          );
-
-    return isUrl(path)
-        ? networkImage()
-        : isAsset(path)
-            ? assetImage()
-            : fileImage();
+    return _isUrl(path)
+        ? _networkImage()
+        : _isAsset(path)
+            ? _assetImage()
+            : _fileImage();
   }
 }
